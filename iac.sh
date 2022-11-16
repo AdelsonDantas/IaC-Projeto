bccd330928b8733567ed51bd18506c75ae19be14
AdelsonDantas <118361237+AdelsonDantas@users.noreply.github.com>
2022-11-16 11:56:44 -0300

    Update iac.sh

--- iac.sh
+++ iac.sh
@@ -16,53 +16,53 @@ echo -e "Diretório criado: sec\n"
 
 echo -e "Criando grupos de usuários ...\n"
 
-groupadd GRP_ADM
-echo "Grupo criado: GRP_ADM"
+groupadd grp_adm
+echo "Grupo criado: grp_adm"
 
-groupadd GRP_VEN
-echo "Grupo criado: GRP_VEN"
+groupadd grp_ven
+echo "Grupo criado: grp_ven"
 
-groupadd GRP_SEC
-echo -e "Grupo criado: GRP_SEC\n"
+groupadd grp_sec
+echo -e "Grupo criado: grp_sec\n"
 
 echo -e "Criando usuários do grupo de administração ...\n"
 
-useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G grp_adm
 echo "Usuário criado: carlos"
 
-useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G grp_adm
 echo "Usuário criado: maria"
 
-useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G grp_adm
 echo -e "Usuário criado: joao\n"
 
 echo -e "Criando usuários do grupo de vendas ...\n"
 
-useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G grp_ven
 echo "Usuário criado: debora"
 
-useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G grp_ven
 echo "Usuário criado: sebastiana"
 
-useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G grp_ven
 echo -e "Usuário criado: roberto\n"
 
 echo -e "Criando usuários do grupo de segurança ...\n"
 
-useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G grp_sec
 echo "Usuário criado: josefina"
 
-useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G grp_sec
 echo "Usuário criado: amanda"
 
-useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G grp_sec
 echo -e "Usuário criado: rogerio\n"
 
 echo -e "Adicionando usuários aos grupos ...\n"
 
-chown root:GRP_ADM /adm
-chown root:GRP_VEN /ven
-chown root:GRP_SEC /sec
+chown root:grp_adm /adm
+chown root:grp_ven /ven
+chown root:grp_sec /sec
 
 echo -e "Especificando permissões ...\n"
 
